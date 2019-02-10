5c75e2189c41d6b14ad7de5e8f50fbb3ab471f47
Michael Snider <massnider@gmail.com>
2019-02-10 16:05:27 -0500

    removed untracked files

--- html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['deviation',['deviation',['../hw__fixed_8cc.html#a9cfdbe00bb990c14c3a9e59a7dbbc974',1,'hw_fixed.cc']]]
-];
--- html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['hw_5ffixed_2ecc',['hw_fixed.cc',['../hw__fixed_8cc.html',1,'']]]
-];
