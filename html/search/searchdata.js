0c5c10f64770818f41d463170eb36c585bbd2072
Michael Snider <massnider@gmail.com>
2019-02-10 03:08:40 -0500

    Added doxygen comments

--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -1,18 +1,24 @@
 var indexSectionsWithContent =
 {
-  0: "a",
-  1: "a"
+  0: "dhs",
+  1: "h",
+  2: "d",
+  3: "s"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "pages"
+  1: "files",
+  2: "functions",
+  3: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Pages"
+  1: "Files",
+  2: "Functions",
+  3: "Pages"
 };
 
